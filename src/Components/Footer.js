6e69d931ee945e668e904bd94b4560f2d4697936
Meghan Sharma <officialmeghan1@gmail.com>
2026-09-08 21:59:40 +0530

    added footer changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,14 +1,38 @@
 import React from 'react';
-import { Box, Typography, Stack, Link } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+
+import Logo from '../assets/images/Logo-1.png';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <Box sx={{ bgColor: '#f8f9fa', pt: 4, pb: 3, mt: 5 }}>
-      <Typography variant="body2" color="text.secondary" align="center" sx={{ mb: 2 }}>
-        {'© '}
-        {new Date().getFullYear()}
-        {"| Fitness App. All rights reserved."}
+    <Box
+      sx={{
+        bgcolor: '#f8f9fa',
+        pt: 4,
+        pb: 3,
+        mt: 5,
+        textAlign: 'center',
+      }}
+    >
+      <Typography
+        variant="body2"
+        color="text.secondary"
+        align="center"
+        sx={{ mb: 2 }}
+      >
+        © {new Date().getFullYear()} | Fitness App. All rights reserved.
       </Typography>
+      <Link to='/'>
+      <img
+        src={Logo}
+        alt="Fitness App logo"
+        style={{
+          width: '10rem',
+          height: 'auto',
+        }}
+      />
+      </Link>
     </Box>
   );
 };
